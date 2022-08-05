9ee53a2332b7df4112cc449c96ae07a1b3dc4f4c
A-C-Sai <64731868+A-C-Sai@users.noreply.github.com>
2022-08-05 21:28:49 +1000

    Finished Component

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
 // Start writing JavaScript here!
+const menuBtn = document.querySelector("button");
+
+const body = document.body;
+
+menuBtn.addEventListener("click", function (e) {
+  body.classList.toggle("offsite-is-open");
+});
